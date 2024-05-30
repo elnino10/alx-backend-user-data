0772a99d6d5f060074940e1d8cb4ccb665fced02
Joe <joeegboka@gmail.com>
2024-05-30 21:18:54 +0100

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -86,8 +86,7 @@ def main() -> None:
         msg = (
             "name={}; email={}; phone={}; ssn={}; "
             "password={}; ip={}; last_login={}; user_agent={};"
-        ).format(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7]
-                )
+        ).format(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7])
         logger.info(msg)
     cursor.close()
     db.close()
