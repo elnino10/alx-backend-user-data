f0ce0f9eafcb51ebf46bacc9f4d3270561f4ce05
Joe Egboka <joeegboka@gmail.com>
2024-06-15 00:07:41 +0100

    format auth module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -108,5 +108,7 @@ class Auth:
         user = self._db.find_user_by(reset_token=reset_token)
         if not user:
             raise ValueError
-        hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
-        self._db.update_user(user.id, hashed_password=hashed_password, reset_token=None)
+        hashed_password = bcrypt.hashpw(
+            password.encode("utf-8"), bcrypt.gensalt())
+        self._db.update_user(
+            user.id, hashed_password=hashed_password, reset_token=None)
